94489abb0760d988b73994129bb75ce34c8ea4d0
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-11-03 19:12:32 +0530

    created controllers

--- src/main/java/com/recipe/recipe/controller/FavoriteController.java
+++ src/main/java/com/recipe/recipe/controller/FavoriteController.java
@@ -1,4 +1,44 @@
 package com.recipe.recipe.controller;
 
+
+
+
+import com.recipe.recipe.entity.Recipe;
+import com.recipe.recipe.entity.User;
+import com.recipe.recipe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users/{userId}/favorites")
 public class FavoriteController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> addFavorite(@PathVariable Long userId, @RequestBody Recipe recipe) {
+        Optional<User> userOpt = userService.getUserById(userId);
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            userService.addFavoriteRecipe(user, recipe);
+            return ResponseEntity.ok("Recipe added to favorites");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getFavorites(@PathVariable Long userId) {
+        Optional<User> userOpt = userService.getUserById(userId);
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            return ResponseEntity.ok(user.getFavorites());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+    }
 }
+
--- src/main/java/com/recipe/recipe/controller/RecipeController.java
+++ src/main/java/com/recipe/recipe/controller/RecipeController.java
@@ -1,4 +1,31 @@
 package com.recipe.recipe.controller;
 
+
+import com.recipe.recipe.dto.RecipeResponse;
+import com.recipe.recipe.entity.Recipe;
+import com.recipe.recipe.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recipes")
 public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Recipe>> searchRecipes(@RequestParam String query) {
+        List<Recipe> recipes = recipeService.searchRecipes(query);
+        return ResponseEntity.ok(recipes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RecipeResponse> getRecipeDetails(@PathVariable Long id) {
+        RecipeResponse recipe = recipeService.getRecipeDetails(id);
+        return ResponseEntity.ok(recipe);
+    }
 }
